77f322a30874f6c147dbac9b87b5324647135395
KhanTagiev <xan.2011@yandex.ru>
2021-08-29 13:07:05 +0300

    refactor: delete extra HTML tags and delete unused Form component

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -2,26 +2,24 @@ import React from "react";
 
 import "./FilterCheckbox.css";
 
-function FilterCheckbox({ isChecked, onChange, isWasRequest }) {
+function FilterCheckbox({isChecked, onChange, isWasRequest}) {
   return (
-    <>
-      <label className="filter__label">
-        <input
-          type="checkbox"
-          className="filter__checkbox"
-          checked={isChecked}
-          onChange={onChange}
-          disabled={!isWasRequest}
-        />
-        <p
-          className={`filter__text ${
-            isWasRequest ? "" : "filter__text_disabled"
-          }`}
-        >
-          Короткометражки
-        </p>
-      </label>
-    </>
+    <label className="filter__label">
+      <input
+        type="checkbox"
+        className="filter__checkbox"
+        checked={isChecked}
+        onChange={onChange}
+        disabled={!isWasRequest}
+      />
+      <p
+        className={`filter__text ${
+          isWasRequest ? "" : "filter__text_disabled"
+        }`}
+      >
+        Короткометражки
+      </p>
+    </label>
   );
 }
 
